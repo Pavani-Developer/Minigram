664d039c0d0a7f110e36ac3a5697b142a7fda319
Pavani-Developer <pavanipampana0404@gmail.com>
2024-10-26 17:30:47 +0530

    .

--- minigram_backend/api/models.py
+++ minigram_backend/api/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
-class RegisterUser(models.Model):
-    email = models.EmailField()
-    username= models.CharField(max_length=100)
-    password = models.CharField(max_length=100)
-    confirm_password = models.CharField(max_length=100)
+class UserProfiles(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True, null=True)
+    image = models.ImageField(upload_to='profiles/', blank=True, null=True)
 
     def __str__(self):
-        return self.username
-    
-
+        
+        return self.user.username
--- minigram_backend/api/serializers.py
+++ minigram_backend/api/serializers.py
@@ -3,11 +3,6 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class RegisterUserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RegisterUser
-        fields = '__all__'
-        
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
