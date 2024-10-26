02deb6e57028df4581967277c6f4b6a721e917f7
Pavani-Developer <pavanipampana0404@gmail.com>
2024-10-26 17:07:46 +0530

    .

--- minigram_backend/api/models.py
+++ minigram_backend/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,13 +12,4 @@ class RegisterUser(models.Model):
     def __str__(self):
         return self.username
     
-# class User(models.Model):
-#     registration = models.OneToOneField(User, on_delete=models.CASCADE)
-#     first_name = models.CharField(max_length=50, blank=True)
-#     last_name = models.CharField(max_length=50, blank=True)
-#     bio = models.TextField(blank=True)
-#     image = models.ImageField(upload_to='profiles/', blank=True)
 
-#     def __str__(self):
-#         return self.registration.username
-    
