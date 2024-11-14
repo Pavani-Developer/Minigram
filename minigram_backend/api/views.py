a87d9d76e28ba97acd9f1330c97ad31f9bd4c05e
Pavani-Developer <pavanipampana0404@gmail.com>
2024-11-14 12:00:36 +0530

    .

--- minigram_backend/api/views.py
+++ minigram_backend/api/views.py
@@ -147,4 +147,3 @@ def get_single_post(request,id):
     except Exception as e:
         return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
-    #kdhfwbksmdb cksjefvwefwsdvs 
