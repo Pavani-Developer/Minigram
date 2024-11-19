25ca91a148afa2cb67c6c5b98c99bfd4fcfdee42
Pavani-Developer <pavanipampana0404@gmail.com>
2024-11-19 18:39:26 +0530

    .

--- minigram-frontend/src/Pages/Feed.js
+++ minigram-frontend/src/Pages/Feed.js
@@ -47,7 +47,7 @@ const Feed = () => {
 
     fetchPosts();
   }, []);
-//REACT_APP_BATCH_ATTENDANCE_API
+
   const toggleComments = (postId) => {
     setCommentsVisible((prev) => ({
       ...prev,
