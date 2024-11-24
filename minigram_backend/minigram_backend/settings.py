a656ce95bb831868932e46c2c17cb088a75289b4
Pavani-Developer <pavanipampana0404@gmail.com>
2024-11-24 20:00:39 +0530

    .

--- minigram_backend/minigram_backend/settings.py
+++ minigram_backend/minigram_backend/settings.py
@@ -170,8 +170,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-   
     "http://localhost:3000",
   
 ]
-ALLOWED_HOSTS = ['https://pavanipampana.pythonanywhere.com/', "http://localhost:3000"]
+ALLOWED_HOSTS = [ "http://localhost:3000"]
